3b5aaff3c5297312b91e0dce833889d2267a7633
Felipe Delgado <af.delgador@uniandes.edu.co>
2021-07-02 16:09:13 -0500

    arrelo parentesis

--- index.js
+++ index.js
@@ -18,39 +18,41 @@ function getRandomCard() {
     } else {
         return randomNumer
     }
-    function startGame() {
-        isAlive = true
-        let firstCard = getRandomCard()
-        let secondCard = getRandomCard()
-        cards = [firstCard, secondCard]
-        sum = firstCard + secondCard
-        renderGame()
+
+}
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
     }
 
-    function renderGame() {
-        cardsEl.textContent = "Cards: "
-        for (let i = 0; i < cards.length; i++) {
-            cardsEl.textContent += cards[i] + " "
-        }
-
-        sumEl.textContent = "Sum: " + sum
-        if (sum <= 20) {
-            message = "Do you want to draw a new card?"
-        } else if (sum === 21) {
-            message = "You've got Blackjack!"
-            hasBlackJack = true
-        } else {
-            message = "You're out of the game!"
-            isAlive = false
-        }
-        messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
     }
+    messageEl.textContent = message
+}
 
 
-    function newCard() {
-        let card = getRandomCard()
-        sum += card
-        cards.push(card)
-        console.log(cards)
-        renderGame()
-    }
+function newCard() {
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
+}
